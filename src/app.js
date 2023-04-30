645405df74041264087335d1115300e39e9fd6d6
Owis <owisbukhari@gmail.com>
2023-04-30 13:54:50 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(5500, () => {
+  console.log("Server is running on port 5500");
+});
+
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+//sub domain /name
+app.get("/name", (req, res) => {
+  res.send("welcome my name is owis bukahri");
+});
